772f65a23ad99f9cca7c9b8298df8789de33a0b1
shilangyu <xmarcinmarcin@gmail.com>
2025-03-02 02:14:12 +0100

    Implement list theorems

--- untyped/Fos/Datatypes.lean
+++ untyped/Fos/Datatypes.lean
@@ -21,8 +21,19 @@ def and : Term :=
 def not : Term :=
   ```λb -> b {bfalse} {btrue}```
 
-theorem boolean_expr_simple :=
-  sorry
+theorem boolean_expr_simple : ```{not} ({and} {btrue} {bfalse})``` ~~>* btrue := by
+  calc
+    _ ~~> ```{and} {btrue} {bfalse} {bfalse} {btrue}``` := by constructor
+    _ ~~> ```(λb -> {btrue} b {bfalse}) {bfalse} {bfalse} {btrue}``` := by repeat constructor
+    _ ~~> ```{btrue} {bfalse} {bfalse} {bfalse} {btrue}``` := by
+      apply Reduce.app1
+      apply Reduce.app1
+      apply Reduce.appAbs
+      -- Q: why does repeat constructor not work here?
+    _ ~~> ```(λf -> {bfalse}) {bfalse} {bfalse} {btrue}``` := by repeat constructor
+    _ ~~> ```{bfalse} {bfalse} {btrue}``` := by repeat constructor
+    _ ~~> ```(λf -> f) {btrue}``` := by repeat constructor
+    _ ~~> btrue := by repeat constructor
 
 -- Arithmetic
 
@@ -46,22 +57,39 @@ theorem iszero_succ : elaborate (λ "n" => {iszero}({succ}("n"))) ~~>* elaborate
 
 -- Fold lists
 
+def pair :=
+  ```λa -> λb -> λf -> f a b```
+
 def flist_nil :=
-  sorry
+  ```λx -> {btrue}```
 
 def flist_cons :=
-  sorry
+  ```λe -> λc -> {pair} e c```
 
 def flist_isnil :=
-  sorry
+  ```λp -> p (λa -> λb -> {bfalse})```
 
 theorem flist_isnil_nil : elaborate ({flist_isnil}({flist_nil})) ~~>* btrue := by
-  sorry
+  calc
+    _ ~~> ```{flist_nil} (λa -> λb -> {bfalse})``` := by constructor
+    _ ~~> btrue := by constructor
 
 theorem flist_isnil_cons :
   elaborate (λ "h" => λ "t" => {flist_isnil}({flist_cons}("h")("t")))
   ~~>* elaborate (λ "h" => λ "t" => {bfalse}) := by
-  sorry
+  apply reduce_many_abs
+  apply reduce_many_abs
+  calc
+    _ ~~> ((elaborate' ["t", "h"] ({flist_cons}("h")("t"))).t_app ```λa -> λb -> {bfalse}```) := by constructor
+    _ ~~> ((elaborate' ["t", "h"] ((λ "c" => {pair}("h")("c"))("t"))).t_app ```λa -> λb -> {bfalse}```) := by repeat constructor
+    _ ~~> ((elaborate' ["t", "h"] ({pair}("h")("t"))).t_app ```λa -> λb -> {bfalse}```) := by
+      apply Reduce.app1
+      apply Reduce.appAbs
+    _ ~~> ((elaborate' ["t", "h"] ((λ "b" => λ "f" => "f"("h")("b"))("t"))).t_app ```λa -> λb -> {bfalse}```) := by repeat constructor
+    _ ~~> ((elaborate' ["t", "h"] ((λ "f" => "f"("h")("t")))).t_app ```λa -> λb -> {bfalse}```) := by repeat constructor
+    _ ~~> (elaborate' ["t", "h"] ((λ "a" => λ "b" => {bfalse})("h")("t"))) := by repeat constructor
+    _ ~~> (elaborate' ["t", "h"] ((λ "b" => {bfalse})("t"))) := by repeat constructor
+    _ ~~> bfalse := by repeat constructor
 
 def flist_head :=
   sorry
